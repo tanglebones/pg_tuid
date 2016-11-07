de2890f7ff9881e0863126335ec6e7143bff4203
Cliff Hammerschmidt <cliff.hammerschmidt@wealthbar.com>
2016-11-07 14:58:51 -0800

    add shmem and locking

--- tuid.c
+++ tuid.c
@@ -7,6 +7,10 @@
 #include "utils/uuid.h"
 #include "utils/timestamp.h"
 #include "storage/lwlock.h"
+#include "storage/lmgr.h"
+#include "storage/ipc.h"
+#include "storage/shmem.h"
+#include "miscadmin.h"
 
 /* Q. What is this?
  * A. A TUID is like a UUID (it conforms to UUID v4) but instead of being fully random it is prefixed with
@@ -30,11 +34,7 @@ typedef struct tuid_state {
     unsigned int seq;
 } tuid_state_t;
 
-static tuid_state_t tuid_state = {
-    NULL,
-    0,
-    0
-};
+static tuid_state_t * tuid_state;
 static int __node_id = 0;
 
 /* Q. Why does seq exist?
@@ -51,6 +51,19 @@ static int __node_id = 0;
 
 PG_FUNCTION_INFO_V1(tuid_generate);
 void _PG_init(void);
+void _PG_fini(void);
+static Size tuid_state_memsize(void);
+static void tuid_shmem_startup(void);
+
+static Size
+tuid_state_memsize(void) {
+    Size size;
+    size = MAXALIGN(sizeof(tuid_state_t));
+    return size;
+}
+static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
+
+#define TUID_SHMEM_NAME "pg_tuid"
 
 void _PG_init(void)
 {
@@ -68,6 +81,35 @@ void _PG_init(void)
         NULL,
         NULL
     );
+    RequestAddinShmemSpace(tuid_state_memsize());
+    RequestAddinLWLocks(1);
+    prev_shmem_startup_hook = shmem_startup_hook;
+    shmem_startup_hook = tuid_shmem_startup;
+
+}
+
+void _PG_fini(void)
+{
+    if (!process_shared_preload_libraries_in_progress)
+        return;
+    shmem_startup_hook = prev_shmem_startup_hook;
+}
+
+static void tuid_shmem_startup()
+{
+    bool found;
+    tuid_state = NULL;
+    if (prev_shmem_startup_hook) {
+        prev_shmem_startup_hook();
+    }
+    LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
+    tuid_state = ShmemInitStruct(TUID_SHMEM_NAME, sizeof(tuid_state_t),&found);
+    if (!found) {
+        tuid_state->last=0;
+        tuid_state->seq=0;
+        tuid_state->lock=LWLockAssign();
+    }
+    LWLockRelease(AddinShmemInitLock);
 }
 
 // based on GetCurrentIntegerTimestamp but without the POSTGRES_EPOCH_JDATE shift
@@ -86,19 +128,20 @@ tuid_generate(PG_FUNCTION_ARGS)
     unsigned int seq;
     unsigned short node_id = __node_id;
 
-    // need a lock around this
-    if (t_us <= tuid_state.last) {
-        if (tuid_state.seq > TUID_MAX_SEQ) {
-            seq = tuid_state.seq =0;
-            last = ++tuid_state.last;
+    LWLockAcquire(tuid_state->lock, LW_EXCLUSIVE);
+    if (t_us <= tuid_state->last) {
+        if (tuid_state->seq > TUID_MAX_SEQ) {
+            seq = tuid_state->seq =0;
+            last = ++tuid_state->last;
         } else {
-            seq = ++tuid_state.seq;
-            last = tuid_state.last;
+            seq = ++tuid_state->seq;
+            last = tuid_state->last;
         }
     } else {
-        seq = tuid_state.seq = 0;
-        last = tuid_state.last = t_us;
+        seq = tuid_state->seq = 0;
+        last = tuid_state->last = t_us;
     }
+    LWLockRelease(tuid_state->lock);
 
     // 01234567-0123-0123-0123-0123456789ab\0
     // 1234567890123456789012345678901234567 890
