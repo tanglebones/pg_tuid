f65c4050e9a8418d60e855e440980b89ec512e2c
Cliff Hammerschmidt <z+gh@hammerschmidt.ca>
2020-04-18 10:29:30 -0700

    Mask r0
    Prevent r0 high bits from corrupting dx.

--- psql/tuid.sql
+++ psql/tuid.sql
@@ -26,7 +26,7 @@ BEGIN
   ax := ct >> 32;
   bx := ct >> 16 & x'FFFF' :: INT;
   cx := x'4000' :: INT | ((ct >> 4) & x'0FFF' :: INT);
-  dx := x'8000' :: INT | ((ct & x'F' :: INT) << 10) | (r0 >> 6);
+  dx := x'8000' :: INT | ((ct & x'F' :: INT) << 10) | ((r0 & x'3F'::INT) >> 6);
 
   ret :=
     LPAD(TO_HEX(ax),8,'0') ||
@@ -35,47 +35,6 @@ BEGIN
     LPAD(TO_HEX(dx),4,'0') ||
     LPAD(TO_HEX(r1),4,'0') ||
     LPAD(TO_HEX(r2),8,'0');
-
-  return ret :: UUID;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION tuid_old_generate()
-  RETURNS UUID AS $$
-DECLARE
-  ct  BIGINT;
-  seq BIGINT;
-  nid BIGINT;
-  r0  BIGINT;
-  r1  BIGINT;
-  ax  BIGINT;
-  bx  BIGINT;
-  cx  BIGINT;
-  dx  BIGINT;
-  ex  BIGINT;
-  fx  BIGINT;
-  ret VARCHAR;
-BEGIN
-  seq := 255;
-  nid := 255;
-  ct := extract(EPOCH FROM clock_timestamp() AT TIME ZONE 'utc') * 1000000;
-  r0 := (random() * 4294967295 :: BIGINT) :: BIGINT;
-  r1 := (random() * 4294967295 :: BIGINT) :: BIGINT;
-
-  ax := ct >> 32;
-  bx := ct >> 16 & x'FFFF' :: INT;
-  cx := x'4000' :: INT | ((ct >> 4) & x'0FFF' :: INT);
-  dx := x'8000' :: INT | ((ct & x'F' :: INT) << 10) | (seq << 2) | (nid >> 6);
-  ex := ((nid & x'3F' :: INT) << 2) | (r0 & x'3FF' :: INT);
-  fx := r1;
-
-  ret :=
-    LPAD(TO_HEX(ax),8,'0') ||
-    LPAD(TO_HEX(bx),4,'0') ||
-    LPAD(TO_HEX(cx),4,'0') ||
-    LPAD(TO_HEX(dx),4,'0') ||
-    LPAD(TO_HEX(ex),4,'0') ||
-    LPAD(TO_HEX(fx),8,'0');
 
   return ret :: UUID;
 END;
@@ -109,7 +68,7 @@ BEGIN
   ax := ct >> 32;
   bx := ct >> 16 & x'FFFF' :: INT;
   cx := x'4000' :: INT | ((ct >> 4) & x'0FFF' :: INT);
-  dx := x'8000' :: INT | ((ct & x'F' :: INT) << 10) | (r0 >> 6);
+  dx := x'8000' :: INT | ((ct & x'F' :: INT) << 10) | ((r0 & x'3F'::INT) >> 6);
 
   ret :=
     LPAD(TO_HEX(ax),8,'0') ||
